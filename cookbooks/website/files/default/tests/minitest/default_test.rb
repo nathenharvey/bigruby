939595017958a03ab216228fbc87101109acd916
nathenharvey <nharvey@opscode.com>
2013-03-01 00:48:12 -0600

    Add minitests

---
+++ cookbooks/website/files/default/tests/minitest/default_test.rb
@@ -0,0 +1,29 @@
+class TestWebsite < MiniTest::Chef::TestCase
+  include MiniTest::Chef::Assertions
+  include MiniTest::Chef::Context
+  include MiniTest::Chef::Resources
+
+  def test_succeed
+    assert run_status.success?
+  end
+  
+  def test_that_the_package_installed
+    package("apache2").must_be_installed
+  end
+
+  def test_that_the_service_is_running
+    service("apache2").must_be_running
+  end
+
+  def test_that_the_service_is_enabled
+    service("apache2").must_be_enabled
+  end
+
+  def test_home_page_file
+    file("/var/www/index.html").must_exist
+  end
+
+  def test_home_page_content
+    file("/var/www/index.html").must_include node['conference']['name']
+  end
+end
--- cookbooks/website/metadata.rb
+++ cookbooks/website/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'nharvey@opscode.com'
 license          'All rights reserved'
 description      'Installs/Configures website'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
